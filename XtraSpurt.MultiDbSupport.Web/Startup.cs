2462be050b6c28ebe9781b46b6b8b01703dcaf15
tlejmi <tarek.lejmi@outlook.com>
2020-11-21 23:33:20 +0100

    docker compose file

--- XtraSpurt.MultiDbSupport.Web/Startup.cs
+++ XtraSpurt.MultiDbSupport.Web/Startup.cs
@@ -1,21 +1,37 @@
+using System.Text.Json.Serialization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using XtraSpurt.MultiDbSupport.Commons;
 
 
 namespace XtraSpurt.MultiDbSupport.Web
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
+        private readonly IWebHostEnvironment _environment;
+        private IConfiguration _configuration;
+        public Startup(IConfiguration configuration, IWebHostEnvironment environment)
         {
+            _environment = environment;
+            _configuration = configuration;
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc()
+                .AddJsonOptions(options =>
+                {
+                    options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+                    options.JsonSerializerOptions.IgnoreNullValues = true;
+                    options.JsonSerializerOptions.PropertyNamingPolicy = null;
+                });
+            services.AddXtraSpurtDependency(_environment);
+        }
+
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -24,13 +40,14 @@ namespace XtraSpurt.MultiDbSupport.Web
             }
 
             app.UseRouting();
+            app.UseStaticFiles(); 
+
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllerRoute(
+                    "default",
+                    "{controller=Home}/{action=Index}");
             });
         }
     }
